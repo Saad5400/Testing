0b1c3c9de8abba57b776675aab1f1d6ce30675f3
Saad5400 <86385454+Saad5400@users.noreply.github.com>
2022-09-11 06:00:30 +0300

    Add files via upload

---
+++ CoderHub/Octal Converting/Octal To Binary/Program.cs
@@ -0,0 +1,35 @@
+﻿string oct_to_bin(int octal) {
+    // multiply first number by 8 to the power of the zeros of that digit, and add up
+    // octal | firstNum | operation | decimal
+    // 3400  | 3        | * 8^3     | 1536
+    //  400  | 4        | * 8^2     | 1792
+    //   00  | 0        | * 8^1     | 1792
+    //    0  | 0        | * 8^0     | 1792
+    int decim = 0;
+    string octalAsStr = octal.ToString();
+    int octalLength = octalAsStr.Length;
+    int index = octalLength - 1;
+
+    foreach (char c in octalAsStr) {
+        decim += (int) (Char.GetNumericValue(c) * Math.Pow(8, index));
+        index--;
+    }
+    return Convert.ToString(decim, 2);
+}
+    
+// for testing the function
+while (true) {
+    try
+    {
+        string input = Console.ReadLine();
+        if (input == "exit")
+            break;
+        Console.WriteLine(oct_to_bin(Convert.ToInt32(input)));
+    }
+    catch (System.Exception e)
+    {
+        
+        Console.WriteLine(e);
+    }
+}
+    
---
+++ CoderHub/Postfix/Program.cs
@@ -0,0 +1,57 @@
+﻿static int postFix(string expr) {
+    int x = 0, y = 0;
+    int result = 0;
+    string operators = "+-/*";
+    string[] exprArray = expr.Split(' ');
+
+    foreach (string str in exprArray) {
+        if (operators.Contains(str)) {
+            switch (str) {
+                case "+":
+                result = x + y;
+                break;
+
+                case "-":
+                result = x - y;
+                break;
+
+                case "*":
+                result = x * y;
+                break;
+
+                case "/":
+                result = x / y;
+                break;
+            }
+            x = result;
+            y = 0;
+        }
+        else {
+            if (x == 0) {
+                x = Convert.ToInt32(str);
+            }
+            else {
+                y = Convert.ToInt32(str);
+            }
+        }
+    }
+    return result;
+}
+    
+    
+// for testing the function
+while (true) {
+    try
+    {
+        string input = Console.ReadLine();
+        if (input == "exit")
+            break;
+        Console.WriteLine(postFix(input));
+    }
+    catch (System.Exception e)
+    {
+        
+        Console.WriteLine(e);
+    }
+}
+    
